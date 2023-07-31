e51ed2e8ef0c15a0c0947d729a4dcd9c7288fd2a
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-07-31 10:46:08 +0300

    Added user Dashboard

--- Angular-project/src/app/views/dashboard/dashboard.component.ts
+++ Angular-project/src/app/views/dashboard/dashboard.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {  Component, OnInit} from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+    
+
+    constructor (public articleService: ArticleService) {}
+
+    ngOnInit(): void {
+      setTimeout(() => {
+        const userId: any = localStorage.getItem('user')
+        const {name, id} = JSON.parse(userId);
+        
+        this.articleService.fetchArticles(id) 
+      }, 500)
+               
+    }
+
+    
 
 }
