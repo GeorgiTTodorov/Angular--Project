c751c58986641f0ab906ac81bc610b09d5eb0907
Georgi Todorov <106379153+GeorgiTTodorov@users.noreply.github.com>
2023-07-31 10:48:17 +0300

    Added collection component

--- Angular-project/src/app/views/collection/collection.component.ts
+++ Angular-project/src/app/views/collection/collection.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
-export class CollectionComponent {
+export class CollectionComponent implements OnInit {
 
+    constructor (public articleService: ArticleService ) {
+
+    }
+  
+    ngOnInit(): void {
+        setTimeout(() => {
+
+          this.articleService.fetchAllArticles();
+        }, 500)
+    }
 }
